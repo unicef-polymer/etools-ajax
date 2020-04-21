29aeb36e831bfea9aa1f09260f170d4868fc7c4c
Adriana Trif <trif.corina.adriana@gmail.com>
2020-04-21 23:10:09 +0300

    d.ts files

---
+++ ajax-error-parser.d.ts
@@ -0,0 +1,7 @@
+export declare function tryGetResponseError(response: any): any;
+export declare function getErrorsArray(errors: any): any[];
+export declare function formatServerErrorAsText(error: any): string;
+export declare function parseRequestErrorsAndShowAsToastMsgs(error: any, source: HTMLElement, redirectOn404?: boolean);
+export declare function showErrorAsToastMsg(errorsString: string, source: HTMLElement)
+
+
---
+++ upload-helper.d.ts
@@ -0,0 +1,4 @@
+export declare function getActiveXhrRequests(): any;
+export declare async function upload(config: UploadConfig, rawFile: File | Blob, filename: string): Promise<any>;
+export declare function abortActiveRequests(activeReqKeys: string[]);
+
