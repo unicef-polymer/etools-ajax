94becc04f47c11e16de9e093dbb738cfd2f8b543
Uladzimir Urbanovich <v_urbanovich@mail.ru>
2021-08-09 14:45:06 +0300

    Change existed code

--- ajax-error-parser.js
+++ ajax-error-parser.js
@@ -13,84 +13,68 @@ export function tryGetResponseError(response) {
   return response.response || globalMessage;
 }
 
+/**
+ *
+ * @param errors
+ * @returns {string[]}
+ */
 export function getErrorsArray(errors) {
-  // @ts-ignore
-  const errorsArray = [];
   if (!errors) {
-    // @ts-ignore
-    return errorsArray;
+    return [];
   }
 
   if (typeof errors === 'string') {
-    errorsArray.push(errors);
-    return errorsArray;
-  }
-  if (typeof errors === 'object' && errors.error && typeof errors.error === 'string') {
-    errorsArray.push(errors.error);
-    return errorsArray;
+    return [errors];
   }
 
-  if (typeof errors === 'object' && errors.errors && Array.isArray(errors.errors)) {
-    errors.errors.forEach(function (err) {
+  const isObject = typeof errors === 'object';
+  if (isObject && errors.error && typeof errors.error === 'string') {
+    return [errors.error];
+  }
+
+  if (isObject && errors.errors && Array.isArray(errors.errors)) {
+    return errors.errors.map(function (err) {
       if (typeof err === 'object') {
-        const errKeys = Object.keys(err);
-        if (errKeys.length > 0) {
-          errKeys.forEach(function (k) {
-            errorsArray.push(err[k]); // will work only for strings
-          });
-        }
+        return Object.values(err); // will work only for strings
       } else {
-        errorsArray.push(err);
+        return err;
       }
-    });
-    return errorsArray;
+    }).flat();
   }
 
-  if (typeof errors === 'object' && errors.non_field_errors && Array.isArray(errors.non_field_errors)) {
-    [].push.apply(errorsArray, errors.non_field_errors);
-    return errorsArray;
+  if (isObject && errors.non_field_errors && Array.isArray(errors.non_field_errors)) {
+    return errors.non_field_errors;
   }
 
-  if (Array.isArray(errors) && errors.length > 0 && _isArrayOfStrings(errors)) {
-    Array.prototype.push.apply(errorsArray, errors);
-    return errorsArray;
+  if (Array.isArray(errors) && _isArrayOfStrings(errors)) {
+    return errors;
   }
 
-  if (typeof errors === 'object' && Object.keys(errors).length > 0) {
-    let errField;
-    for (errField in errors) { // eslint-disable-line
-
-      if (typeof errors[errField] === 'string') {
-        errorsArray.push('Field ' + errField + ' - ' + errors[errField]);
-        continue;
-      }
-      if (Array.isArray(errors[errField]) && errors[errField].length > 0) {
-        let parentErr = 'Field ' + errField + ': ';
-        let nestedErrs = getErrorsArray(errors[errField]);
-        if (nestedErrs.length === 1) {
-          parentErr += nestedErrs[0];
-          errorsArray.push(parentErr);
-        } else {
-          errorsArray.push(parentErr);
+  if (isObject) {
+    return Object
+      .entries(errors)
+      .map(([field, value]) => {
+        if (typeof value === 'string') {
+          return `Field ${field} - ${value}`;
+        }
+        if (Array.isArray(value)) {
+          const baseText = `Field ${field}: `;
+          const textErrors = getErrorsArray(value);
           // * The marking is used for display in etools-error-messages-box
           // * and adds a welcomed identations when displayed as a toast message
-          nestedErrs = _markNestedErrors(nestedErrs);
-          Array.prototype.push.apply(errorsArray, nestedErrs);
+          return textErrors.length === 1 ? `${baseText}${textErrors}` : [baseText, ..._markNestedErrors(textErrors)];
         }
-        continue;
-      }
-      if (typeof errors[errField] === 'object' && Object.keys(errors[errField]).length > 0) {
-        let errF;
-        for (errF in errors[errField]) {
-          if (errors[errField][errF]) {
-            errorsArray.push('Field ' + errField + '(' + errF + ') - ' + getErrorsArray(errors[errField][errF]));
-          }
+        if (typeof value === 'object') {
+          return Object
+            .entries(value)
+            .map(([nestedField,  ]) =>
+              `Field ${field} (${nestedField}) - ${getErrorsArray(nestedValue)}`);
         }
-      }
-    }
+      })
+      .flat();
   }
 
-  return errorsArray;
+  return [];
 }
 
 function _markNestedErrors(errs) {
